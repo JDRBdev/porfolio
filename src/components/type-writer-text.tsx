9911b248c90301e8781b9b83421c3394ce90480e
JoseDavid-2daw <josezondaw@gmail.com>
2025-05-07 22:45:49 +0100

    primer commit

---
+++ src/components/type-writer-text.tsx
@@ -0,0 +1,17 @@
+import Typewriter from 'typewriter-effect';
+
+function TypewriterText({ className = '' }) {
+  return (
+    <div className={`text-white text-4 ${className}`}>
+      <Typewriter
+        options={{
+          strings: ['Desarrollador Web', 'Diseñador Web', 'Programador'],
+          autoStart: true,
+          loop: true,
+        }}
+      />
+    </div>
+  );
+}
+
+export default TypewriterText;
